a6b021857bcf76ab930b7dd980ce045a8473afc9
James Easter <james.m.easter@gmail.com>
2025-04-07 22:58:41 -0600

    Update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,5 @@
 import Home from "../pages/Home.vue";
-import Contact from "../pages/Contact.vue";
 import Unknown from "../pages/Unknown.vue";
-import Gallery from "../pages/Gallery.vue";
-import Services from "../pages/Services.vue";
 import EquipmentRepair from "../pages/EquipmentRepair.vue";
 import CustomFabrication from "../pages/CustomFabrication.vue";
 import ResidentialWelding from "../pages/ResidentialWelding.vue";
@@ -10,12 +7,9 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", name: "Home", component: Home },
-  { path: "/gallery", name: "Gallery", component: Gallery },
-  { path: "/services", name: "Services", component: Services },
-  { path: "/services/custom-fabrication", name: "Custom Fabrication", component: CustomFabrication },
-  { path: "/services/equipment-repair", name: "Equipment Repair", component: EquipmentRepair },
-  { path: "/services/residential-welding", name: "Residential Welding", component: ResidentialWelding },
-  { path: "/contact", name: "Contact", component: Contact },
+  { path: "/custom-fabrication", name: "Custom Fabrication", component: CustomFabrication },
+  { path: "/equipment-repair", name: "Equipment Repair", component: EquipmentRepair },
+  { path: "/residential-welding", name: "Residential Welding", component: ResidentialWelding },
   { path: '/:pathMatch(.*)*', name: "Unknown", component: Unknown },
 ];
 
@@ -23,6 +17,15 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+        top: 40,
+      }
+    }
+  }
 });
 
 export default router;
