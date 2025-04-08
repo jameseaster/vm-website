05725eefd08f575026d3440cc6a397814abec353
James Easter <james.m.easter@gmail.com>
2025-04-07 22:57:17 -0600

    Update routes and add images to constants.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,43 +1,57 @@
 export const headerHeight = 65;
 
+export const galleryImages = [
+  new URL("../assets/gallery/IMG_0247.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_0757.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_1161.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_1192.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_0219.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_0220.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_1306.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_1308.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_1440.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_1590.png", import.meta.url).href,
+  new URL("../assets/gallery/IMG_1902.png", import.meta.url).href,
+];
+
 export const headerItems = [
-  {
-    id: "home",
-    type: 'button',
-    route: '/',
-    label: "Home",
-    image: new URL("../assets/mock-welding-photo.jpeg", import.meta.url).href,
-    children: []
-  },
   {
     id: "services",
     type: 'menu',
-    route: "/services",
+    route: "/#services",
     label: "Services",
-    image: new URL("../assets/deck.png", import.meta.url).href,
+    // image: new URL("../assets/deck.png", import.meta.url).href,
     children: [
+      {
+        id: "all",
+        type: "menu-item",
+        label: "All Services",
+        route: "/#services",
+        // image: new URL("../assets/deck.png", import.meta.url).href,
+        children: []
+      },
       {
         id: "custom-fabrication",
         type: "menu-item",
         label: "Custom Fabrication",
-        route: "/services/custom-fabrication",
-        image: new URL("../assets/deck.png", import.meta.url).href,
+        route: "/#custom-fabrication",
+        // image: new URL("../assets/deck.png", import.meta.url).href,
         children: []
       },
       {
         id: "equipment-repair",
         type: "menu-item",
         label: "Equipment Repair",
-        route: "/services/equipment-repair",
-        image: new URL("../assets/mock-welding-photo.jpeg", import.meta.url).href,
+        route: "/#equipment-repair",
+        // image: new URL("../assets/mock-welding-photo.jpeg", import.meta.url).href,
         children: []
       },
       {
         id: "residential-welding",
         type: "menu-item",
         label: "Residential Welding",
-        route: "/services/residential-welding",
-        image: new URL("../assets/stairs.png", import.meta.url).href,
+        route: "/#residential-welding",
+        // image: new URL("../assets/stairs.png", import.meta.url).href,
         children: []
       },
     ]
@@ -45,17 +59,17 @@ export const headerItems = [
   {
     id: "gallery",
     type: 'button',
-    route: "/gallery",
+    route: "/#gallery",
     label: "Gallery",
-    image: new URL("../assets/stairs.png", import.meta.url).href,
+    // image: new URL("../assets/stairs.png", import.meta.url).href,
     children: []
   },
   {
     id: "contact",
     type: 'button',
-    route: "/contact",
+    route: "/#contact",
     label: "Contact",
-    image: new URL("../assets/house.png", import.meta.url).href,
+    // image: new URL("../assets/house.png", import.meta.url).href,
     children: []
   },
 ];
